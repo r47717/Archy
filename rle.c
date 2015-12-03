3ddd755de35a6627a13436cc6505fe7311de2272
r47717 <mchr@yandex.ru>
2015-12-03 23:29:34 +0300

    updated config

---
+++ rle.c
@@ -0,0 +1,13 @@
+/*
+ * rle.c
+ *
+ *  Created on: 3 дек. 2015 г.
+ *      Author: r47717
+ */
+
+
+void encode_RLE(char *input, char *output) {
+
+
+
+}
---
+++ rle.h
@@ -0,0 +1,13 @@
+/*
+ * rle.h
+ *
+ *  Created on: 3 дек. 2015 г.
+ *      Author: r47717
+ */
+
+#ifndef RLE_H_
+#define RLE_H_
+
+void encode_RLE(char *input, char *output);
+
+#endif /* RLE_H_ */
